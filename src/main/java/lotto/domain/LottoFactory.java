07b7d40142ab6682babea18d3c037eb076fbb4e8
goeka <goeka1337@gmail.com>
2025-11-02 06:33:56 +0900

    refactor(domain): LottoFactory로 클래스명 변경 및 정렬 책임 추가
    - 기존 LottoGenerator를 LottoFactory로 클래스명 변경
    - LottoFactory에서 번호 생성 후 오름차순 정렬까지 수행하도록 함
    - Lotto 클래스는 생성자 내부의 정렬 로직을 제거

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -2,18 +2,22 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class LottoGenerator {
+public class LottoFactory {
     private static final int LOTTO_NUMBER_COUNT = 6;
     private static final int LOTTO_MIN_NUMBER = 1;
     private static final int LOTTO_MAX_NUMBER = 45;
 
     public Lotto generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
-                LOTTO_MIN_NUMBER,
-                LOTTO_MAX_NUMBER,
-                LOTTO_NUMBER_COUNT
-        );
+                        LOTTO_MIN_NUMBER,
+                        LOTTO_MAX_NUMBER,
+                        LOTTO_NUMBER_COUNT
+                ).stream()
+                .sorted()
+                .collect(Collectors.toList());
+
         return new Lotto(numbers);
     }
 }
