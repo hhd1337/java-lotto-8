f00688795d160b03e6a2d35ac381aa004ff421c6
goeka <goeka1337@gmail.com>
2025-11-03 08:08:16 +0900

    feat(domain): LottoFactory에 여러 Lotto 객체를 생성,반환하는 메소드 추가

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,7 +10,18 @@ public class LottoFactory {
     private static final int LOTTO_MIN_NUMBER = 1;
     private static final int LOTTO_MAX_NUMBER = 45;
 
-    public Lotto generate() {
+    public List<Lotto> generateMany(int count) {
+        validateCount(count);
+
+        List<Lotto> tickets = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            tickets.add(generateOne());
+        }
+
+        return tickets;
+    }
+
+    public Lotto generateOne() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
                         LOTTO_MIN_NUMBER,
                         LOTTO_MAX_NUMBER,
@@ -19,5 +31,11 @@ public class LottoFactory {
                 .collect(Collectors.toList());
 
         return new Lotto(numbers);
+    }
+
+    private void validateCount(int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 생성 개수는 1개 이상이어야 합니다.");
+        }
     }
 }
