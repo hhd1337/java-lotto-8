84340a08c86e7677fdbb332ed20611189c678937
goeka <goeka1337@gmail.com>
2025-11-03 19:47:42 +0900

    refactor(domain): Lotto 생성자 검증 로직을 LottoNumbersValidator로 위임
    -Lotto클래스 내부에 있던 번호개수,범위,중복 검증로직을 외부 Validator 클래스로 이동시켜 책임 분리함.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,22 +1,19 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.support.validator.LottoNumbersValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoNumbersValidator lottoNumbersValidator = new LottoNumbersValidator();
+        lottoNumbersValidator.validate(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
     }
 
     public List<Integer> getNumbers() {
         return numbers;
     }
+
 }
