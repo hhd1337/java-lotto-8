33b331dcbacaad3db27724f142ba22c81d898c89
goeka <goeka1337@gmail.com>
2025-11-03 18:49:55 +0900

    fix(view): OutputView의 출력 형식을 가이드 예시에 맞게 줄바꿈 처리
    - 우테코 3주차 로또 미션 가이드의 출력 예시와 동일한 줄바꿈 구조를 적용함.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,19 +3,23 @@ package lotto.view;
 import java.util.List;
 
 public class OutputView {
+
     public void printPurchaseAmountNotice() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
     public void printWinningNumbersNotice() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
     public void printBonusNumberNotice() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
     public void printPurchasedLottoCount(int count) {
+        System.out.println();
         System.out.printf("%d개를 구매했습니다.", count);
     }
 
@@ -24,6 +28,7 @@ public class OutputView {
     }
 
     public void printWinResultHeader() {
+        System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---");
     }
@@ -33,6 +38,6 @@ public class OutputView {
     }
 
     public void printProfitRate(double profitRate) {
-        System.out.printf("총 수익률은 %.1f%%입니다.", profitRate);
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
     }
 }
