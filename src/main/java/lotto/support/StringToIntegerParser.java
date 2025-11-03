82339f525834862b2cb3baad13d1ee9e32862a90
goeka <goeka1337@gmail.com>
2025-11-03 10:32:40 +0900

    feat(support): 문자열을 정수로 변환하는 StringToIntegerParser 클래스 추가

---
+++ src/main/java/lotto/support/StringToIntegerParser.java
@@ -0,0 +1,11 @@
+package lotto.support;
+
+public class StringToIntegerParser {
+    public int parseToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.");
+        }
+    }
+}
